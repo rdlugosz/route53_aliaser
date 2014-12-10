431f1e9b47abdd918b70cf00bc60a6165fc4352c
Ryan Dlugosz <ryan@dlugosz.net>
2014-12-10 16:16:19 -0500

    Fix #3: Raise error if engine polled on target_record

--- app/controllers/route53_aliaser/aliaser_controller.rb
+++ app/controllers/route53_aliaser/aliaser_controller.rb
@@ -1,6 +1,9 @@
 module Route53Aliaser
   class AliaserController < ActionController::Base
     def update
+      if request.host == Route53Aliaser.config.target_record
+        raise "Route53Updater: Update requested on URL of target_zone. Please make request via source_zone."
+      end
       Route53Aliaser.update_alias_if_needed
       head :ok
     end
